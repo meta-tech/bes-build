5fcea7dc9b9bce4c318cdb13aba418bccda08a67
a-sansara <dev@pluie.org>
2017-04-30 02:16:30 +0200

    adding build.sh

--- src/main.sh
+++ src/main.sh
@@ -1,49 +1,18 @@
 #!/bin/bash
 
 BES_BUILD_VERSION=0.2
-
-bes.title 'bes-build' $BES_BUILD_VERSION
-
-  APP_DIR=$(pwd)
- APP_NAME=$(basename $(pwd)) 
-  APP_BIN=$APP_DIR/dist/$APP_NAME
+          APP_DIR=$(pwd)
+         APP_NAME=$(basename $(pwd)) 
+          APP_BIN=$APP_DIR/dist/$APP_NAME
 
 bes.main(){
-
+    bes.title 'bes-build' $BES_BUILD_VERSION
     echo
-    bes.echo.title "building project" "$APP_NAME"
-    if [ -d "$APP_DIR/src" ]; then 
-        if [ ! -d "$APP_DIR/dist" ]; then
-            bes.echo.action "creating dist directory"
-            mkdir $APP_DIR/dist
-            bes.echo.state $?
-        fi
-        if [ -f "$APP_BIN" ]; then
-            bes.echo.action "removing ${Coff}dist/$APP_NAME${Coff}"
-            rm $APP_BIN
-            bes.echo.state $?
-        fi
-        echo "#!/bin/bash" > $APP_BIN
-        bes.echo.action "reading ${Coff}src/"
-        for entry in "$APP_DIR/src"/*.sh; do
-            if [ "$(basename $entry)" != "main.sh" ]; then
-                bes.echo "      - appending ${Coff}src/$(basename $entry)"
-                tail -n +2 "$entry" >> "$APP_BIN"
-            fi
-        done
-        if [ -f "$APP_DIR/src/main.sh" ]; then
-            tail -n +2 "$APP_DIR/src/main.sh" >> "$APP_BIN"
-            bes.echo "      - appending ${Coff}src/main.sh"
-        fi
-        bes.echo.state 0
-        bes.echo.action "set execution mode"
-        chmod +x $APP_BIN
-        bes.echo.state $?
-    else
-        bes.echo.error "no src/ directory. exit"
-        bes.echo.state 1
+    if   [ "$1" = "install" ]; then
+        bes.install
+    elif [ -z "$1" ]; then
+        bes.build "$1"
     fi
-    echo
 }
 
 bes.main $*
